be02f1ad3e1142393baa29a3bbee6ad3749310ad
Jason <Nail-it-Jason@users.noreply.github.com>
2024-11-07 21:02:40 +0800

    chap2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-std::string global_str;
-int global_int;
 int main() {
-    int local_int;
-    std::string local_str;
-    std::cout << global_str << " " << global_int << ' ' << local_int << ' ' << local_str;
+    int i = 100, sum = 0;
+    for (int i = 0; i != 10; ++i)
+        sum += i;
+    std::cout << i << " " << sum << std::endl;
+
 }
