e07d62f016591df5552877ec582e21191ec6e741
Jason <Nail-it-Jason@users.noreply.github.com>
2024-11-11 23:06:27 +0800

    chap2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 int main() {
-    int i = 100, sum = 0;
-    for (int i = 0; i != 10; ++i)
-        sum += i;
-    std::cout << i << " " << sum << std::endl;
-
+    int i = 0;
+//    double *dp = &i;
+//    int *ip = i;
+    int *p = &i;
 }
